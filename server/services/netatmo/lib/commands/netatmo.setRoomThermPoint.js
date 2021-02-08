f67816380bf52f08dfe77e03e23adf7dd6735ed9
Terdious <thomas.lemaistre76@gmail.com>
2021-02-08 13:08:30 +0100

    Eslint et Prettier

--- server/services/netatmo/lib/commands/netatmo.setRoomThermPoint.js
+++ server/services/netatmo/lib/commands/netatmo.setRoomThermPoint.js
@@ -14,11 +14,11 @@ async function setRoomThermPoint(homeId, roomId, modeThermostat, valueThermostat
     logger.debug(`Netatmo : New consigne thermostat : ${modeThermostat}`);
     const consThermostat = valueThermostat + 3;
     const options = {
-      'home_id': homeId,
-      'room_id': roomId,
-      'mode': modeThermostat,
-      'temp': consThermostat,
-      'access_token': this.token,
+      home_id: homeId,
+      room_id: roomId,
+      mode: modeThermostat,
+      temp: consThermostat,
+      access_token: this.token,
     };
     await axios.post(`${this.baseUrl}/api/setroomthermpoint`, options);
   } catch (err) {
